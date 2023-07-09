e263f1d4d6f5186cf03d012cb29fffa653392369
Ken Atopos <z1158229761@outlook.com>
2023-07-09 21:31:06 +0800

    design place order page

--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+
 const apiClient = axios.create({
   baseURL:
     process.env.NODE_ENV === "development" ? "http://localhost:4000/" : "/",
@@ -6,5 +7,20 @@ const apiClient = axios.create({
     "Content-type": "application/json",
   },
 }); // create a new instance of axios client; baseUrl and headers are custom configuration
+
+apiClient.interceptors.request.use(
+  // interceptor is a function that axios will run before each request or response
+  async (config) => {
+    if (localStorage.getItem("userInfo"))
+      config.headers.authorization = `Bearer ${
+        JSON.parse(localStorage.getItem("userInfo")!).token
+      }`;
+
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
 
 export default apiClient;
